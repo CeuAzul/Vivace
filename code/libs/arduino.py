ad97b8d1a59bd3ed46be22d07a1cff69c218db80
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2018-06-29 18:45:44 -0300

    Remove multiple port connection and insert port on object creation
    The previous way consisted of trying to connect on several ports, but this will not work when both an Arduino *and* a Serial Transmitter are connected. To prevent future problems, now the Arduino class receives a port as a parameter, with a default one being used if not.
    
    Also the entire atualiza() code  was put inside a try/excpet block, in case no Arduino is connected.

--- code/libs/arduino.py
+++ code/libs/arduino.py
@@ -19,36 +19,24 @@ class Arduino:
     ao sensor de ultrassom e o tacometro.
     """
 
-    def __init__(self, baudrate=115200):
+    def __init__(self, porta='/dev/ttyUSB0', baudrate=115200):
         """Construtor: Inicializa o objeto da classe e inicializa a
         comunicaçao serial via porta USB1, com baudrate igual a 9600bps.
         """
 
         self.codificacao = "utf-8"
         self.baudrate = baudrate
+        self.porta = porta
         self.dicioDeDados = dict()
+        self.connect()
 
-        print("########## Trying Arduino on port ttyUSB0! ##########")
+    def connect(self):
+        print("########## Trying Arduino on port " + self.porta + "! ##########")
         try:
-            self.ser = serial.Serial(port='/dev/ttyUSB0', baudrate=self.baudrate, timeout=1)
-            print("########## Arduino connected! ##########")
+            self.ser = serial.Serial(port=self.porta, baudrate=self.baudrate, timeout=1)
+            print("########## Arduino connected on port " + self.porta + "! ##########")
         except:
-            print("########## Trying Arduino on port ttyUSB1! ##########")
-            try:
-                self.ser = serial.Serial(port='/dev/ttyUSB1', baudrate=self.baudrate, timeout=1)
-                print("########## Arduino connected! ##########")
-            except:
-                print("########## Trying Arduino on port ttyUSB2! ##########")
-                try:
-                    self.ser = serial.Serial(port='/dev/ttyUSB2', baudrate=self.baudrate, timeout=1)
-                    print("########## Arduino connected! ##########")
-                except:
-                    print("########## Trying Arduino on port ttyACM0! ##########")
-                    try:
-                        self.ser = serial.Serial(port='/dev/ttyACM0', baudrate=self.baudrate, timeout=1)
-                        print("########## Arduino connected! ##########")
-                    except:
-                        sys.exit("########## Arduino not detected! ##########")
+            print("########## Arduino not detected on port " + self.porta + "! ##########")
 
     def updateData(self):
         """Puxa linha de dados pela porta serial (Arduino).
@@ -59,22 +47,22 @@ class Arduino:
 
         try:
             linha_de_dados = self.ser.readline().decode(self.codificacao)
+
+            if(linha_de_dados != ""):
+                linha_de_dados = linha_de_dados.replace("\r\n", "")
+                if linha_de_dados.startswith("!") and linha_de_dados.endswith("@"):
+                    linha_de_dados = linha_de_dados.replace(" ", "")
+                    linha_de_dados = linha_de_dados.replace("!", "")
+                    linha_de_dados = linha_de_dados.replace("@", "")
+                    dados = linha_de_dados.split(";")
+                    for dado in dados:
+                        try:
+                            apelido, valor = dado.split("=")
+                            self.dicioDeDados[apelido] = float(valor)
+                        except:
+                            pass
         except:
             pass
-
-        if(linha_de_dados != ""):
-            linha_de_dados = linha_de_dados.replace("\r\n", "")
-            if linha_de_dados.startswith("!") and linha_de_dados.endswith("@"):
-                linha_de_dados = linha_de_dados.replace(" ", "")
-                linha_de_dados = linha_de_dados.replace("!", "")
-                linha_de_dados = linha_de_dados.replace("@", "")
-                dados = linha_de_dados.split(";")
-                for dado in dados:
-                    try:
-                        apelido, valor = dado.split("=")
-                        self.dicioDeDados[apelido] = float(valor)
-                    except:
-                        pass
 
     def sendCommand(self, comando):
 
