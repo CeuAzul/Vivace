9c86d426915f2b3a618253bb77dfcdd4ac882de0
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2018-10-20 13:46:14 -0300

    Add code for testing transmission
    This code can be run to test the transmission. It can be run and the app will be able to plot time and sin function.

---
+++ code/transmission_tester.py
@@ -0,0 +1,46 @@
+from libs.telepatia import Transmissor
+from thredeiro import Thredeiro
+from datetime import datetime
+import time
+import math
+
+
+initialTime = datetime.now()
+timeNow = 0
+
+index = 0
+sinIndex = 0
+
+def atualizar_valores(delay):
+    global index
+    global sinIndex
+    global timeNow
+
+    while True:
+        index += 1
+        if index >= 360:
+            index = 0
+
+        sinIndex = math.sin(math.radians(index))
+
+        now = datetime.now()
+        timeNow = (now - initialTime).total_seconds()
+
+        time.sleep(delay)
+
+def enviar_pacotes(delay):
+    print('Transmitindo')
+    while True:
+        transmissor.transmiteDadoProtocolado("htb", 1)
+        transmissor.transmiteDadoProtocolado("tmt", 1)
+        transmissor.transmiteDadoProtocolado("gvd", 1)
+        transmissor.transmiteDadoProtocolado("cfg", 1)
+        transmissor.transmiteDadoProtocolado("idx", index)
+        transmissor.transmiteDadoProtocolado("sin", sinIndex)
+        transmissor.transmiteDadoProtocolado("tmp", timeNow)
+        time.sleep(delay)
+
+transmissor = Transmissor(",", True, 57600, 'UTF-8')
+
+threadAtualizacao = Thredeiro('Thread de atualizacao de dados', atualizar_valores, 0.02)
+threadEnvio = Thredeiro('Thread de envio de pacotes', enviar_pacotes, 0.05)
